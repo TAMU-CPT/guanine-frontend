55559bf0917a9a8ac0b092b88835acc56e4fc606
Eleni Mijalis <elenimijalis@gmail.com>
2016-10-03 13:32:57 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,141 @@
+window._ = require('lodash');
+require('angular');
+require('angular-route');
+require('restangular');
+require('angular-resource');
+require('angular-material');
+require('angular-material-icons');
+require('angular-aria');
+require('angular-gravatar');
+require('angular-material-data-table');
+require('angular-messages');
+require('angular-animate');
+require('jquery');
+require('ngstorage');
+require('angular-jwt');
+require('ns-popover');
+var moment = require('moment');
+
+var guanineApp = angular.module('guanineApp', [
+    'ngRoute',
+    'restangular',
+    'ngMdIcons',
+    'ngMaterial',
+    'ui.gravatar',
+    'ngMessages',
+    'ngAnimate',
+    'md.data.table',
+    'nsPopover',
+    'ngStorage' // https://github.com/gsklee/ngStorage
+]);
+
+guanineApp.config(['$routeProvider', '$httpProvider', '$mdThemingProvider', 'gravatarServiceProvider', 'RestangularProvider',
+    function($routeProvider, $httpProvider, $mdThemingProvider, gravatarServiceProvider, RestangularProvider) {
+        gravatarServiceProvider.defaults = {
+          size     : 100,
+          "default": 'mm'  // Mystery man as default for missing avatars
+        };
+        gravatarServiceProvider.secure = true;
+        gravatarServiceProvider.protocol = 'my-protocol';
+        $mdThemingProvider.theme('default')
+            .primaryPalette('blue')
+            .accentPalette('pink');
+        $routeProvider.
+            when('/courses', {
+                templateUrl: 'partials/course-list.html',
+                controller: 'CourseListCtrl'
+            }).
+            //when('/courses/:courseID', {
+                //templateUrl: 'partials/course-detail.html',
+                //controller: 'CourseDetailCtrl'
+            //}).
+            //when('/students', {
+                //templateUrl: 'partials/student-list.html',
+                //controller: 'StudentListCtrl'
+            //}).
+            //when('/students/:studentID', {
+                //templateUrl: 'partials/student-detail.html',
+                //controller: 'StudentDetailCtrl'
+            //}).
+            //when('/login', {
+                //templateUrl: 'partials/login.html',
+                //controller: 'LoginCtrl'
+            //}).
+            //when('/logout', {
+                //templateUrl: 'partials/login.html',
+                //controller: 'LogOutCtrl'
+            //}).
+            //when('/help', {
+                //templateUrl: 'partials/help.html',
+                //controller: 'HelpCtrl'
+            //}).
+            when('/', {
+                templateUrl: 'partials/home.html',
+                controller: 'HomeCtrl'
+            }).
+            otherwise({
+                redirectTo: '/'
+            });
+
+            //function loginRequired($q, $location, $localStorage) {
+                //var deferred = $q.defer();
+                //if ($localStorage.jwtToken) {
+                    //deferred.resolve();
+                //} else {
+                    //$location.path('/login');
+                //}
+                //return deferred.promise;
+            //};
+        RestangularProvider.setBaseUrl('http://localhost:8000/');
+        RestangularProvider.setRequestSuffix('/');
+        RestangularProvider.addResponseInterceptor(function(data, operation, what, url, response, deferred) {
+            var extractedData;
+            // .. to look for getList operations
+            if (operation === "getList") {
+            // .. and handle the data and meta data
+                extractedData = data.results;
+                extractedData.meta = {
+                    'count': data.count,
+                    'next': data.next,
+                    'previous': data.previous
+                }
+            } else {
+                extractedData = data;
+            }
+            return extractedData;
+        });
+
+        $httpProvider.interceptors.push(['$q', '$location', '$localStorage', function ($q, $location, $localStorage) {
+            return {
+               'request': function (config) {
+                   config.headers = config.headers || {};
+                   if ($localStorage.jwtToken) {
+                       config.headers.Authorization = 'JWT ' + $localStorage.jwtToken;
+                   }
+                   return config;
+               },
+               'responseError': function (response) {
+                   console.log('Failed with', response.status, 'status');
+                   if (response.status == 401 || response.status == 403 || response.status == 400 || response.status == 500) {
+                       console.log('bad');
+                       //$location.path('/login');
+                   }
+                   return $q.reject(response);
+               }
+           };
+        }]);
+}]);
+
+require('./directives.js')(guanineApp);
+require('./factory.js')(guanineApp);
+require('./filter.js')(guanineApp);
+require('./service.js')(guanineApp);
+require('./const.js')(guanineApp);
+require('./ctrl/home.js')(guanineApp);
+require('./ctrl/course/list.js')(guanineApp);
+require('./ctrl/course/detail.js')(guanineApp);
+require('./ctrl/student/list.js')(guanineApp);
+require('./ctrl/student/detail.js')(guanineApp);
+require('./ctrl/login.js')(guanineApp);
+require('./ctrl/logout.js')(guanineApp);
+require('./ctrl/help.js')(guanineApp);
