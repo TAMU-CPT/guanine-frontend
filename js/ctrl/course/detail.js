a50a2a8ca7a77c36f20267f72bd54416c8d45b51
Eleni Mijalis <elenimijalis@gmail.com>
2016-10-21 16:00:19 -0500

    student table

--- js/ctrl/course/detail.js
+++ js/ctrl/course/detail.js
@@ -4,5 +4,16 @@ export default function(guanineApp) {
             Restangular.one('courses', $routeParams.courseID).get().then(function(data) {
                 $scope.course = data;
             });
+
+            $scope.options = {
+                limitSelect: true,
+                pageSelect: true
+            };
+
+            $scope.query = {
+                limit: 5,
+                page: 1,
+                order: 'name',
+            };
     }]);
 }
