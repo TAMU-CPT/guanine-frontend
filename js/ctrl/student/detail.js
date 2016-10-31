f905eb265abb0f16dc232b13320f95ea7616541e
Eleni Mijalis <elenimijalis@gmail.com>
2016-10-31 15:03:21 -0500

    student detail view @erasche

--- js/ctrl/student/detail.js
+++ js/ctrl/student/detail.js
@@ -1,8 +1,41 @@
+var moment = require('moment');
+
 export default function(guanineApp) {
     guanineApp.controller('StudentDetailCtrl', ['$scope', 'Restangular', '$location', '$routeParams',
         function($scope, Restangular, $location, $routeParams) {
             Restangular.one('students', $routeParams.studentID).get().then(function(data) {
                 $scope.student = data;
             });
+
+            $scope.ordering="assessment__title";
+
+            $scope.updateData = function(page) {
+                if(!isNaN(parseInt(page))){
+                    $scope.query.page = page;
+                }
+                $scope.query.ordering = $scope.ordering;
+                $scope.promise = Restangular.all('results').getList($scope.query).then(function(data) {
+                    $scope.results = data;
+                });
+            };
+
+            $scope.options = {
+                limitSelect: true,
+                pageSelect: true
+            };
+
+            $scope.query = {
+                limit: 50,
+                page: 1,
+                student: $routeParams.studentID,
+                assessment__course: $routeParams.courseID,
+                ordering: $scope.ordering,
+            };
+
+            $scope.updateData(1);
+
+            $scope.date_format = function(date) {
+                return moment(date).format('MM/DD/YYYY');
+            };
     }]);
 }
