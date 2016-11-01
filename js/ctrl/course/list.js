908904b18f689d891e8040e1cfeb54e4fcdc813c
Eleni Mijalis <elenimijalis@gmail.com>
2016-11-01 16:55:49 -0500

    post user data working

--- js/ctrl/course/list.js
+++ js/ctrl/course/list.js
@@ -73,7 +73,6 @@ export default function(guanineApp) {
                 }, function() {
                     $mdLoginToast.show('Invalid student table');
                 });
-
             };
     }]);
 }
