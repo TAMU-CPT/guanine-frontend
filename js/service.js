f709f046c7921ee49ef361d243fc844509d277b5
Eric Rasche <rasche.eric@gmail.com>
2016-11-29 23:03:49 +0000

    Ensure stringified

--- js/service.js
+++ js/service.js
@@ -1,13 +1,15 @@
 export default function(guanineApp) {
-    guanineApp.service('$mdLoginToast', function($mdToast) {
-        return {
-            show: function(content) {
-            return $mdToast.show(
-              $mdToast.simple()
-                .content(content)
-                .position('top right')
-                .hideDelay(2000)
-            )}
-        };
-    });
+	guanineApp.service('$mdLoginToast', ['$mdToast',
+		function($mdToast) {
+			return {
+				show: function(content) {
+				return $mdToast.show(
+				  $mdToast.simple()
+					.content(content)
+					.position('top right')
+					.hideDelay(2000)
+				)}
+			};
+		}
+	]);
 }
